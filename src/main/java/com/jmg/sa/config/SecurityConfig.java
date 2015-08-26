3a4047dde3b8b800cbec735a54be1a0b94a2f3be
javi-more-garc <javi.more.garc@gmail.com>
2015-08-26 17:08:08 +0200

    CHANGE - Added csrf protection

--- src/main/java/com/jmg/sa/config/SecurityConfig.java
+++ src/main/java/com/jmg/sa/config/SecurityConfig.java
@@ -3,7 +3,15 @@
  */
 package com.jmg.sa.config;
 
+import java.io.IOException;
+
 import javax.inject.Inject;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
@@ -14,7 +22,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
 
 /**
  * @author Javier Moreno Garcia
@@ -27,13 +41,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Inject
     private DataSource dataSource;
 
-    // This configuration does not restrict access and does not have csrf
-
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http //
                 .headers().cacheControl().disable().and()//
-                .csrf().disable()//
+                .csrf() //
+                .csrfTokenRepository(csrfTokenRepository()).and().addFilterAfter(csrfHeaderFilter(), CsrfFilter.class)
                 .authorizeRequests() //
                 .antMatchers("/login", "/logout").permitAll()//
                 .anyRequest().authenticated() //
@@ -60,6 +73,38 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    //
+    // private methods
+
+    // the filter is indeed needed (csrf protection can be seen when doing
+    // POSTs)
+
+    private Filter csrfHeaderFilter() {
+        return new OncePerRequestFilter() {
+            @Override
+            protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                    FilterChain filterChain) throws ServletException, IOException {
+                CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+                if (csrf != null) {
+                    Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
+                    String token = csrf.getToken();
+                    if (cookie == null || token != null && !token.equals(cookie.getValue())) {
+                        cookie = new Cookie("XSRF-TOKEN", token);
+                        cookie.setPath("/");
+                        response.addCookie(cookie);
+                    }
+                }
+                filterChain.doFilter(request, response);
+            }
+        };
+    }
+
+    private CsrfTokenRepository csrfTokenRepository() {
+        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+        repository.setHeaderName("X-XSRF-TOKEN");
+        return repository;
     }
 
 }
