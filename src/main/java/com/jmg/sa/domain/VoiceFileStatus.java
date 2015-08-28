86da34cbaba42762963e02dacb4ceb0aa7b5325a
javi-more-garc <javi.more.garc@gmail.com>
2015-08-28 02:09:15 +0200

    CHANGE - Minor changes (comments)

--- src/main/java/com/jmg/sa/domain/VoiceFileStatus.java
+++ src/main/java/com/jmg/sa/domain/VoiceFileStatus.java
@@ -12,13 +12,17 @@ public enum VoiceFileStatus {
 
     // just uploaded to our application
     UPLOADED_TO_APPLICATION, //
+    
     // uploaded to our application and to voice base (we have already media id)
     UPLOADED_TO_VOICE_BASE, //
+    
     // processed in voice base
     PROCESSED_IN_VOICE_BASE, //
-    // processed in voice base, with all details already in our application but
-    // file already exits in voice base
+    
+    // processed in voice base and all its details synchronized in our
+    // application (file still exits in voice base)
     SYNCHRONIZED, //
+    
     // processed (file no longer in voice base)
     PROCESSED, //
 
